81b5917eaeb81ebbb946f8747d05524b9b81954c
nikolaykislov <nikolaykislov888@gmail.com>
2023-03-04 13:29:14 +0300

    fix: переинициализировал приложение в дефолтном лэйауте

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,18 @@
 <template>
-  <Nuxt />
+  <div id="app"
+       :class="$style.default"
+  >
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'default',
+}
+</script>
+
+<style lang="scss" module>
+  .default {
+    //
+  }
+</style>
