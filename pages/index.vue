ca50622abf22462af056491b7a62a410aee27f44
nikolaykislov <nikolaykislov888@gmail.com>
2023-03-04 12:53:19 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.index">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+
+
+}
+</script>
+
+<style lang="scss" module>
+.index {
+  //
+}
+</style>
