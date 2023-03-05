a13acef94d457145e2c8a61ba60e71aa2b4b17f7
nikolaykislov <nikolaykislov888@gmail.com>
2023-03-05 19:30:31 +0300

    feat: добавил оверлей на слайды

--- components/hero/HeroSlider.vue
+++ components/hero/HeroSlider.vue
@@ -68,8 +68,19 @@ export default {
   }
 
   .slide {
+    position: relative;
     width: 100vw;
     height: 100vh;
+
+    &:after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, .1);
+    }
   }
 
   .slideImg {
